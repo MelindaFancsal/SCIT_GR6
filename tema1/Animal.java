890440515677fd4b803da31767ccec282bbbbe42
melinda <Melinda.Fancsal-extern@swisslife.de>
2021-04-28 21:05:23 +0300

    tema1

---
+++ tema1/Animal.java
@@ -0,0 +1,15 @@
+package ierarhie;
+
+public abstract class Animal {
+    protected int picioare;
+
+    protected Animal(int picioare) {
+        this.picioare = picioare;
+    }
+
+    public abstract void mananca();
+
+    public void merge() {
+        System.out.println("animalul cu: " + this.picioare + " picioare merge acum");
+    }
+}
